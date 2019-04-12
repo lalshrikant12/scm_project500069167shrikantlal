091a9f9c342b4267cd45ae4144ea595917795cc4
lalshrikant12 <lalshrikant12gmail.com>
2019-04-12 23:37:25 +0530

    2nd commit

--- project.cpp
+++ project.cpp
@@ -4,8 +4,8 @@ int main()
 {cout<<"select the program you want to run"<<endl;
 	cout<<"1. Use for loop to print the truth table for the expression XY+Z."<<endl;
 	cout<<"2.Convert Decimal number into a Binary Number using 1D array."<<endl;
-
-
+cout<<"3.	Use for loop to print the truth table for the expression XY+Z"
+cout<<"4.	Base class ‘Temperature’ obtains temperature value in Celsius (c) through its parameterized constructor with default arguments and convert it into Fahrenheit value (f) and prints it using the function calculate(). Class ‘Temp’ inherits class ‘Temperature’ to obtain the temperature value in Fahrenheit and converts it into Kelvin value (k) and prints it using the redefined function calculate()."
 
 	int x;
 	cin>>x;
@@ -40,4 +40,53 @@ case2 :    int num, bin;
     break;
 		default :cout<<"invalid choice"<<endl;
 	}
+case3 : 
+ 
+ 
+int main()
+{
+	int x,y,z;
+	cout<<"X\tY\tZ\tXY+Z";
+	
+	for(x=0;x<=1;++x)
+		for(y=0;y<=1;++y)
+			for(z=0;z<=1;++z)
+			{
+				if(x*y+z==2)
+					cout<<"\n\n"<<x<<"\t"<<y<<"\t"<<z<<"\t1";
+				else
+					cout<<"\n\n"<<x<<"\t"<<y<<"\t"<<z<<"\t"<<x*y+z;
+			}
+	
+	return 0;
+}
+case 4: 
 
+class TEMP
+{
+    float f,c;
+   public:
+      float  getdata();
+      void display(void);
+};
+
+ float  TEMP :: getdata()
+{
+cout<<"Enter Value for farenheit degree to find for celsius:- ";
+cin>>f;
+c = (f-32)/1.8;
+return(c);
+}
+
+void TEMP :: display(void)
+{
+float v;
+ v=getdata();
+cout<<"CELSIUS DEGREE = "<<v;
+}
+
+void  main()
+{
+TEMP c;
+c.display();
+}
