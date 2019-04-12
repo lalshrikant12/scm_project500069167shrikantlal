88835afb2d3ea2df32318ca3ca3d993f8d9666cd
lalshrikant12 <lalshrikant12gmail.com>
2019-04-12 23:48:04 +0530

    3RD

--- project.cpp
+++ project.cpp
@@ -6,7 +6,7 @@ int main()
 	cout<<"2.Convert Decimal number into a Binary Number using 1D array."<<endl;
 cout<<"3.	Use for loop to print the truth table for the expression XY+Z"
 cout<<"4.	Base class ‘Temperature’ obtains temperature value in Celsius (c) through its parameterized constructor with default arguments and convert it into Fahrenheit value (f) and prints it using the function calculate(). Class ‘Temp’ inherits class ‘Temperature’ to obtain the temperature value in Fahrenheit and converts it into Kelvin value (k) and prints it using the redefined function calculate()."
-
+cout<<"5.Addition of two matrices using operator overloading - C++"<<endl
 	int x;
 	cin>>x;
 	switch(x)
@@ -90,3 +90,50 @@ void  main()
 TEMP c;
 c.display();
 }
+case 5:class Complex
+{
+        int num1, num2;
+    public:
+        void accept()
+        {
+                cout<<"\n Enter Two Complex Numbers : ";
+                cin>>num1>>num2;
+        }
+
+        //Overloading '+' operator using Friend function
+        friend Complex operator+(Complex c1, Complex c2);   
+                           
+        void display()
+        {
+                cout<<num1<<"+"<<num2<<"i"<<"\n";
+        }
+};
+Complex operator+(Complex c1, Complex c2)
+{
+        Complex c;
+        c.num1=c1.num1+c2.num1;
+        c.num2=c1.num2+c2.num2;
+        return(c);
+}
+int main()
+{
+        Complex c1,c2, sum;      //Created Object of Class Complex i.e c1 and c2
+
+        c1.accept();  //Accepting the values
+        c2.accept();
+
+        sum = c1+c2;   //Addition of object
+    
+        cout<<"\n Entered Values : \n";
+        cout<<"\t";
+        c1.display();    //Displaying user input values
+        cout<<"\t";
+        c2.display();
+  
+        cout<<"\n Addition of Real and Imaginary Numbers : \n";
+        cout<<"\t";
+        sum.display();  //Displaying the addition of real and imaginary numbers
+     
+        return 0;
+}
+
