4ad14481a7d799a9714716be2a81d2cfd314232d
nit29sin <nitish29singla@gmail.com>
2019-04-20 22:25:38 +0530

    added stackusingarray()

--- project.cpp
+++ project.cpp
@@ -1,4 +1,4 @@
-i#include<iostream>
+#include<iostream>
 using namespace std;
 
 void truthtable()
@@ -445,6 +445,70 @@ do {
 } while(ch!=4);
    
 }
+
+void stackusingarray()
+{
+	int stack[100], n=100, top=-1;
+void push(int val) {
+   if(top>=n-1)
+      cout<<"Stack Overflow"<<endl;
+   else {
+      top++;
+      stack[top]=val;
+   }
+}
+void pop() {
+   if(top<=-1)
+      cout<<"Stack Underflow"<<endl;
+   else {
+      cout<<"The popped element is "<< stack[top] <<endl;
+      top--;
+   }
+}
+void display() {
+   if(top>=0) {
+      cout<<"Stack elements are:";
+      for(int i=top; i>=0; i--)
+         cout<<stack[i]<<" ";
+         cout<<endl;
+   } else
+      cout<<"Stack is empty";
+}
+int main() {
+   int ch, val;
+   cout<<"1) Push in stack"<<endl;
+   cout<<"2) Pop from stack"<<endl;
+   cout<<"3) Display stack"<<endl;
+   cout<<"4) Exit"<<endl;
+   do {
+      cout<<"Enter choice: "<<endl;
+      cin>>ch;
+      switch(ch) {
+         case 1: {
+            cout<<"Enter value to be pushed:"<<endl;
+            cin>>val;
+            push(val);
+            break;
+         }
+         case 2: {
+            pop();
+            break;
+         }
+         case 3: {
+            display();
+            break;
+         }
+         case 4: {
+            cout<<"Exit"<<endl;
+            break;
+         }
+         default: {
+            cout<<"Invalid Choice"<<endl;
+         }
+      }
+   }while(ch!=4);
+      return 0;
+}
 }
 int main()
 	
@@ -466,7 +530,7 @@ cout<<"9	Write a generic class to implement the operations of a stack data struc
 cout<<"10	Write contents into a Test file. Get a word from the user as input, compute the number of occurrences of this word in the file and print the count."
 cout<<"11 write a program for array rotation"<<endl;
 cout<<"write a program of implementation of queue using array"<<endl;}
-
+cout<<"C++ Program to Implement Stack using array"<<endl;
 
 int x;
 	cin>>x;
@@ -501,3 +565,5 @@ case 11: arrayrotation();
         break;
 case 12:queuearray()	;
 	break;
+case 13:stackusingarray();
+        break;
