fc97c643a8e773e1396d70615e48fc4345d78713
lalshrikant12 <lalshrikant12gmail.com>
2019-04-20 19:26:33 +0530

    9th fixed

--- project.cpp
+++ project.cpp
@@ -75,38 +75,8 @@ TEMP c;
 c.display();
 }
 
-int main()
-	
-
-{cout<<"select the program you want to run"<<endl;
-	cout<<"1. Use for loop to print the truth table for the expression XY+Z."<<endl;
-	cout<<"2.Convert Decimal number into a Binary Number using 1D array."<<endl;
-cout<<"3.	Use for loop to print the truth table for the expression XY+Z"
-c  out<<"4.	Base class ‘Temperature’ obtains temperature value in Celsius (c) through its parameterized constructor with default arguments and convert it into Fahrenheit value (f) and prints it using the function calculate(). Class ‘Temp’ inherits class ‘Temperature’ to obtain the temperature value in Fahrenheit and converts it into Kelvin value (k) and prints it using the redefined function calculate()."
-cout<<"5.Addition of two matrices using operator overloading - C++"<<endl
-cout<<"7 write a program to print half pyramid 1"<<endl
-                                               1 2
-                                               1 2 3
-					       1 2 3 4
-					       1 2 3 4 5
-cout<<"6.Write a program to multiply a matrix"<<endl;
-}
-
-int x;
-	cin>>x;
-	switch(x)
-	{
-case 1: truthtable();
-       break;
-case 2: decimaltobinary();
-       break;
-case 3: temp();
-       break;}
-
-case 4: temperature();
-        break;
-
-case 5:class Complex
+void complex()
+{class Complex
 {
         int num1, num2;
     public:
@@ -131,28 +101,8 @@ Complex operator+(Complex c1, Complex c2)
         c.num2=c1.num2+c2.num2;
         return(c);
 }
-int main()
-{
-        Complex c1,c2, sum;      //Created Object of Class Complex i.e c1 and c2
 
-        c1.accept();  //Accepting the values
-        c2.accept();
-
-        sum = c1+c2;   //Addition of object
-    
-        cout<<"\n Entered Values : \n";
-        cout<<"\t";
-        c1.display();    //Displaying user input values
-        cout<<"\t";
-        c2.display();
-  
-        cout<<"\n Addition of Real and Imaginary Numbers : \n";
-        cout<<"\t";
-        sum.display();  //Displaying the addition of real and imaginary numbers
-     
-        return 0;
-}
-case 6:int main()
+void matrix()
 {
     int rows;
 
@@ -169,8 +119,9 @@ case 6:int main()
     }
     return 0;
 }
-case 7: int main()
-{
+void matrixmultiplication()
+	{
+   {
     int a[10][10], b[10][10], mult[10][10], r1, c1, r2, c2, i, j, k;
 
     cout << "Enter rows and columns for first matrix: ";
@@ -234,5 +185,44 @@ case 7: int main()
             cout << endl;
     }
 
-    return 0;
+int main()
+	
+
+{cout<<"select the program you want to run"<<endl;
+	cout<<"1. Use for loop to print the truth table for the expression XY+Z."<<endl;
+	cout<<"2.Convert Decimal number into a Binary Number using 1D array."<<endl;
+cout<<"3.	Use for loop to print the truth table for the expression XY+Z"
+c  out<<"4.	Base class ‘Temperature’ obtains temperature value in Celsius (c) through its parameterized constructor with default arguments and convert it into Fahrenheit value (f) and prints it using the function calculate(). Class ‘Temp’ inherits class ‘Temperature’ to obtain the temperature value in Fahrenheit and converts it into Kelvin value (k) and prints it using the redefined function calculate()."
+cout<<"5.Addition of two matrices using operator overloading - C++"<<endl
+cout<<"7 write a program to print half pyramid 1"<<endl
+                                               1 2
+                                               1 2 3
+					       1 2 3 4
+					       1 2 3 4 5
+cout<<"6.Write a program to multiply a matrix"<<endl;
 }
+
+int x;
+	cin>>x;
+	switch(x)
+	{
+case 1: truthtable();
+       break;
+case 2: decimaltoobinary();
+       break;
+case 3: temp();
+       break;}
+
+case 4: temperature();
+        break;
+
+case 5: Complex();
+        break;
+
+        
+case 6: matrix();
+        break;
+
+case 7: matrixmultiplication();
+        break;
+	}
